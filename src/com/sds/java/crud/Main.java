a41a6108ad8ba62bbc5aa5cd1d1a2ad6b8c55ef1
jihey03040 <jihye03040@gmail.com>
2021-07-19 18:59:15 +0900

    게시물 상세페이지(제목,내용,날짜)

--- src/com/sds/java/crud/Main.java
+++ src/com/sds/java/crud/Main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 
-		System.out.println("====프로그램 시작====");
+		System.out.println("=========프로그램 시작=========");
 
 		Scanner sc = new Scanner(System.in);
 
@@ -82,7 +82,7 @@ public class Main {
 			}
 		}
 		sc.close();
-		System.out.println("====프로그램 끝====");
+		System.out.println("=========프로그램 끝=========");
 	}
 }
 
